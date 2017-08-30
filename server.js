8f4d5e2cddfee1271607f920904b42f894ca9f65
anujaminde <30970429+anujaminde@users.noreply.github.com>
2017-08-30 22:19:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -17,6 +19,18 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+function hash (input, salt)
+{
+  var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+  return hashed.toString('hex');
+
+}
+
+app.get('/hash/input:',function(req,res){
+  var hashedString=req(hash.params. input, 'random-string');
+  res.send(hashedString);
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
